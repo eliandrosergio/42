32c3c3f63bcae9271d9329097ee2423d8714ef35
eliandrosergio <150693487+eliandrosergio@users.noreply.github.com>
2023-11-19 06:08:35 +0100

    Add files via upload

---
+++ c-piscina-rush-00/ex00/rush02.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efaustin <marvin@42.fr>                     +#+  +:+       +#+       */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 17:28:12 by efaustin          #+#    #+#             */
+/*   Updated: 2023/11/18 17:30:18 by efaustin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_line2(int x, int a, int b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (x >= 1)
+		ft_putchar(a);
+	while (i <= (x - 3))
+	{
+		ft_putchar(b);
+		i++;
+	}
+	if (x >= 2)
+		ft_putchar(c);
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 0;
+	if (y >= 1 && x >= 1)
+		ft_line2(x, 'A', 'B', 'A');
+	while (i < y - 2 && x >= 1)
+	{
+		ft_line2(x, 'B', ' ', 'B');
+		i++;
+	}
+	if (y >= 2 && x >= 1)
+	{
+		ft_line2(x, 'C', 'B', 'C');
+	}
+}
